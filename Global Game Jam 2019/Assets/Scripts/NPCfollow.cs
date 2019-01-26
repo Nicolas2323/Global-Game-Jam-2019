29c226050d1c93e2305766a69b8aa06b56b8dabe
Nicolas2323 <42986955+Nicolas2323@users.noreply.github.com>
2019-01-26 06:09:59 -0500

    update

---
+++ Global Game Jam 2019/Assets/Scripts/NPCfollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCfollow : MonoBehaviour
+{
+    public float speed = 1f;
+    public GameObject player;
+    public GameObject npc;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        npc = GameObject.Find("player");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        
+
+
+
+
+
+    }
+}
